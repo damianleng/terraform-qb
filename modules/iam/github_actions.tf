a97bceb7bfc08ffa61de6e3b1138f21a9a7be836
damianleng <damianlengdy@gmail.com>
2026-03-06 14:45:24 -0500

    Fix OIDC trust policy

--- modules/iam/github_actions.tf
+++ modules/iam/github_actions.tf
@@ -25,9 +25,8 @@ resource "aws_iam_role" "github_actions" {
         }
         Action = "sts:AssumeRoleWithWebIdentity"
         Condition = {
-          StringEquals = {
-            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
-            "token.actions.githubusercontent.com:sub" = "repo:${var.github_repo}:ref:refs/heads/${var.github_branch}"
+          StringLike = {
+            "token.actions.githubusercontent.com:sub" = "repo:damianleng/terraform-qb:*"
           }
         }
       }
