9b3657189cafd9acffacdffe0f0669f0f51fc617
damianleng <damianlengdy@gmail.com>
2026-03-06 13:50:54 -0500

    commented backend s3

--- terraform.tf
+++ terraform.tf
@@ -9,11 +9,11 @@ terraform {
   required_version = ">= 1.2"
 
   # Uncomment after first successful deployment
-  backend "s3" {
-    bucket         = "qb-financial-warehouse-dev-qb-terraform-state"
-    key            = "terraform.tfstate"
-    region         = "us-east-1"
-    use_lockfile   = true
-    encrypt        = true
-  }
+  # backend "s3" {
+  #   bucket         = "qb-financial-warehouse-dev-qb-terraform-state"
+  #   key            = "terraform.tfstate"
+  #   region         = "us-east-1"
+  #   use_lockfile   = true
+  #   encrypt        = true
+  # }
 }
